5f2873db1f194d21d772f1a56f5f85db946a5a53
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-08-20 20:57:30 -0400

    style format

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Greninja</div>;
+  return <div className="hidden text-sm font-semibold">Greninja</div>;
 }
 
 export default Username;
