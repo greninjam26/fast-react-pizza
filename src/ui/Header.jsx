b55cb63ca8c49165a112c5dc9b6f25b65e6c37b7
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-08-20 20:41:41 -0400

    add colors

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
-	return (
-		<header>
-			<Link to="/">Fast React Pizza Co.</Link>
+  return (
+    <header className="bg-yellow-500">
+      <Link to="/">Fast React Pizza Co.</Link>
 
-			<SearchOrder />
-		</header>
-	);
+      <SearchOrder />
+    </header>
+  );
 }
 
 export default Header;
