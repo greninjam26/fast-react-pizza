86c88745c438748bad4203e9e0a3495a37dfa031
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-08-21 19:54:48 -0400

    bug fix map for cart

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -4,12 +4,12 @@ import DeleteItem from "./DeleteItem";
 import UpdateItemQuantity from "./UpdateItemQuantity";
 import { getCurrentQuantityById } from "./cartSlice";
 
-function CartItem({ item, key }) {
+function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
   const currentQuantity = useSelector(getCurrentQuantityById(pizzaId));
 
   return (
-    <li className=" py-3 sm:flex sm:items-center sm:justify-between" key={key}>
+    <li className=" py-3 sm:flex sm:items-center sm:justify-between">
       <p className="mb-1 sm:mb-0">
         {quantity}&times; {name}
       </p>
